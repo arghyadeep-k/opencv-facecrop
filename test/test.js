97d83c976fefa3343ef63ca0f6997ba171d781d3
arghyadeep <arghyadeep.k@gmail.com>
2020-09-21 16:07:57 +0200

    Test Case added

--- test/test.js
+++ test/test.js
@@ -1,3 +1,17 @@
-const vanessa = require('../index')
+const facecrop = require('../index')
+const fs = require('fs')
 
-vanessa('./test/test-file.jpg', {name: 'output.jpg', type: "image/jpeg", quality: 0.95 })
+async function vanessa(){
+    await facecrop('./test/test-file.jpg', {name: './test/output.jpg', type: "image/jpeg", quality: 0.95 });
+
+    fs.stat("./test/output.jpg",(err,stat) => {
+        if (err == null)
+            console.log('SUCCESS: Test Case Passed.');
+        else if(err.code === 'ENOENT')
+            console.log(err +"\nERROR: File not created. Test Case Failed.");
+        else
+            console.log(err +"\nERROR: Test Case Failed.");
+    });
+}
+
+vanessa();
