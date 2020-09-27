1f42e8c136bbcf8104bb73d4521786d0a4cbb9f8
Arghyadeep <arghyadeep.k@gmail.com>
2020-09-27 22:53:55 +0000

    Enhanced exception handling and new test cases #1

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 const { Canvas, createCanvas, Image, ImageData, loadImage } = require('canvas');
 const { JSDOM } = require('jsdom');
-const { writeFileSync, existsSync, stat, statSync } = require('fs');
-const { exception } = require('console');
+const { writeFileSync, existsSync, statSync } = require('fs');
 
 
 
-module.exports = async (file, name="output.jpg", type="image/jpeg", quality=0.95, factor=0, trainingSet = "./node_modules/opencv-facecrop/resources/haarcascade_frontalface_default.xml") => {  
-    // await loadOpenCV();  
-    let image;
-    let src;
-    let gray;
-    // cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
-    let faces;
-    let faceCascade;
-  try{  
-    await loadOpenCV();  
-    console.log("Loading file...")
-    image = await loadImage(file)    
-    src = cv.imread(image);
+module.exports = async (file, name = "output.jpg", type = "image/jpeg", quality = 0.95, factor = 0, trainingSet = "./node_modules/opencv-facecrop/resources/haarcascade_frontalface_default.xml") => {
+  let image, src, gray, faces, faceCascade;
+  try {
+    await loadOpenCV().catch((e) => { throw new Error("Error: Loading OpenCV failed.\n" + e.message) });
+    console.log("Loading file...");
+    image = await loadImage(file)
+      .catch((e) => { throw new Error("Error: Loading input image failed.\n" + e.message) });
+    if (image != null)
+      src = cv.imread(image);
     gray = new cv.Mat();
     cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY, 0);
     faces = new cv.RectVector();
     faceCascade = new cv.CascadeClassifier();
 
     console.log("Loading pre-trained classifier files...");
-    
-    statSync(trainingSet, (err) => {
-      if(err != null)
-        throw err;
-    });
+
+    try {
+      statSync(trainingSet);
+    }
+    catch (err) {
+      throw new Error("Error: Pre-Trained Classifier file failed to load.\n" + err.message);
+    }
 
     faceCascade.load(trainingSet);
 
     console.log("Processing...")
     let mSize = new cv.Size(0, 0);
     faceCascade.detectMultiScale(gray, faces, 1.1, 3, 0, mSize, mSize);
-    
-    let point1, point2; 
 
-    for (let i = 0; i < faces.size(); ++i) {    
+    let point1, point2;
+
+    for (let i = 0; i < faces.size(); ++i) {
       point1 = new cv.Point(faces.get(i).x, faces.get(i).y);
-      point2 = new cv.Point(faces.get(i).x + faces.get(i).width, faces.get(i).y + faces.get(i).height);       
+      point2 = new cv.Point(faces.get(i).x + faces.get(i).width, faces.get(i).y + faces.get(i).height);
 
       point1.x = point1.x - factor;
       point1.y = point1.y - factor;
@@ -48,39 +45,45 @@ module.exports = async (file, name="output.jpg", type="image/jpeg", quality=0.95
       point2.x = point2.x + factor;
       point2.y = point2.y + factor;
 
+      if (point1.x < 0 || point1.y < 0 || point2.x < 0 || point2.y < 0)
+        throw new Error('Error: Factor passed is too high/low.');
+
       const canvas = createCanvas(point2.x - point1.x, point2.y - point1.y);
-      
+
       let rect = new cv.Rect(point1.x, point1.y, point2.x - point1.x, point2.y - point1.y);
 
-      console.log('Rendering file...')
+      console.log('Rendering output image...')
       let dst = src.roi(rect);
-      
+
       console.log("Source File dimension: " + src.size().width + "x" + src.size().height);
       console.log("Destination File dimension: " + dst.size().width + "x" + dst.size().height);
-          
-      cv.imshow(canvas, dst);    
-      
+
+      cv.imshow(canvas, dst);
+
       let outputFilename = name.toString();
 
-      if(faces.size() > 1){
-        if(outputFilename.charAt(outputFilename.length - 4) == '.'){
-          outputFilename = outputFilename.substr(0, (outputFilename.length - 4)) + `-${i+1}` + outputFilename.substr((outputFilename.length - 4), 4);        
-        }
-        else {
-          throw new Error('File extension should be 3 characters only.');
-        }
+      if (faces.size() > 1) {
+        if (outputFilename.charAt(outputFilename.length - 4) == '.')
+          outputFilename = outputFilename.substr(0, (outputFilename.length - 4)) + `-${i + 1}` + outputFilename.substr((outputFilename.length - 4), 4);
+        else
+          throw new Error('Error: File extension should be 3 characters only.');
       }
 
-      writeFileSync(outputFilename, canvas.toBuffer(type,{ quality: quality }));    
+      writeFileSync(outputFilename, canvas.toBuffer(type, { quality: quality }));
       console.log(outputFilename + " created successfully.");
-    }      
+
+    }
+    return "Success";
   }
-  catch(e){
+  catch (e) {
     console.error(e.message);
-    throw e;
+    return e.message;
   }
-  finally{
-    src.delete(); gray.delete(); faceCascade.delete(); faces.delete(); 
+  finally {
+    if (src) src.delete();
+    if (gray) gray.delete();
+    if (faceCascade) faceCascade.delete();
+    if (faces) faces.delete();
   }
 };
 
@@ -95,7 +98,7 @@ module.exports = async (file, name="output.jpg", type="image/jpeg", quality=0.95
  * @returns {Promise} resolved when the library is ready to use.
  */
 function loadOpenCV(rootDir = '/work', localRootDir = process.cwd()) {
-  if(global.Module && global.Module.onRuntimeInitialized && global.cv && global.cv.imread) {
+  if (global.Module && global.Module.onRuntimeInitialized && global.cv && global.cv.imread) {
     return Promise.resolve()
   }
   return new Promise(resolve => {
@@ -113,22 +116,22 @@ function loadOpenCV(rootDir = '/work', localRootDir = process.cwd()) {
         // do this before the library is executed so the filesystem is accessible from the start
         const FS = global.Module.FS
         // create rootDir if it doesn't exists
-        if(!FS.analyzePath(rootDir).exists) {
+        if (!FS.analyzePath(rootDir).exists) {
           FS.mkdir(rootDir);
         }
         // create localRootFolder if it doesn't exists
-        if(!existsSync(localRootDir)) {
-          mkdirSync(localRootDir, { recursive: true});
+        if (!existsSync(localRootDir)) {
+          mkdirSync(localRootDir, { recursive: true });
         }
         // FS.mount() is similar to Linux/POSIX mount operation. It basically mounts an external
         // filesystem with given format, in given current filesystem directory.
-        FS.mount(FS.filesystems.NODEFS, { root: localRootDir}, rootDir);
+        FS.mount(FS.filesystems.NODEFS, { root: localRootDir }, rootDir);
       }
     };
     global.cv = require('opencv4js')
   });
 }
-function installDOM(){
+function installDOM() {
   const dom = new JSDOM();
   global.document = dom.window.document;
   global.Image = Image;
--- test/test.js
+++ test/test.js
@@ -1,44 +1,61 @@
 const facecrop = require('../index')
 const fs = require('fs')
 
-test('Single face detection', async() => {
-  expect.assertions(1);
-  await facecrop('./test/test-file-1.jpg', './test/output.jpg', "image/jpeg", 0.95, 100, './resources/haarcascade_frontalface_default.xml');
-  return expect(isExists1("./test/output.jpg")).toBeTruthy();
+test('Single face detection', async () => {
+  //expect.assertions(1);
+  await facecrop('./test/test-file-1.jpg', './test/out.jpg', "image/jpeg", 0.95, 100, './resources/haarcascade_frontalface_default.xml');
+  return expect(isExists1("./test/out.jpg")).toBeTruthy();
 });
 
-test('Multiple face detection', async() => {
-  expect.assertions(1);
+test('Multiple face detection', async () => {
+  //expect.assertions(1);
   await facecrop('./test/test-file-2.jpg', './test/output.jpg', "image/jpeg", 0.95, 0, './resources/haarcascade_frontalface_default.xml');
   return expect(isExists2("./test/output-1.jpg", "./test/output-2.jpg")).toBeTruthy();
 });
 
-test('Invalid training set path',  async() => {
-    await expect(facecrop('./test/test-file-1.jpg', './test/out.jpg', "image/jpeg", 0.95))
-    .rejects
-    .toThrow("no such file or directory, stat './node_modules/opencv-facecrop/resources/haarcascade_frontalface_default.xml'");
+test('Return value', async () => {
+  let out = await facecrop('./test/test-file-1.jpg', './test/out.jpg', "image/jpeg", 0.95, 100, './resources/haarcascade_frontalface_default.xml');
+  return expect(out).toMatch('Success');
 });
 
-test('Invalid output extension', async() => {  
-  await expect(facecrop('./test/test-file-2.jpg', './test/output.jpeg', "image/jpeg", 0.95, 0, './resources/haarcascade_frontalface_default.xml'))
-    .rejects
-    .toThrow("File extension should be 3 characters only.")
+test('Invalid input image parameter', async () => {
+  let out = await facecrop('./invalid-file-name');
+  return expect(out).toMatch("Error: Loading input image failed");
 });
 
-async function isExists1(filename){
-  fs.stat(filename,(err) => {    
-    return err == null ? true : false;    
+test('Invalid training set path', async () => {
+  let out = await facecrop('./test/test-file-1.jpg', './test/out.jpg', "image/jpeg", 0.95);
+  return expect(out).toMatch("Pre-Trained Classifier file failed to load.");
+  // .rejects
+  // .toThrow("no such file or directory, stat './node_modules/opencv-facecrop/resources/haarcascade_frontalface_default.xml'");
+});
+
+test('Invalid output extension', async () => {
+  let out = await facecrop('./test/test-file-2.jpg', './test/output.jpeg', "image/jpeg", 0.95, 0, './resources/haarcascade_frontalface_default.xml')
+  return expect(out).toMatch("File extension should be 3 characters only.");
+  // .rejects
+  // .toThrow("File extension should be 3 characters only.")
+});
+
+test('Factor out of bounds', async () => {
+  let out = await facecrop('./test/test-file-1.jpg', './test/output.jpg', "image/jpeg", 0.95, 1000, './resources/haarcascade_frontalface_default.xml');
+  return expect(out).toMatch("Factor passed is too high/low.");
+});
+
+async function isExists1(filename) {
+  fs.stat(filename, (err) => {
+    return err == null ? true : false;
   });
 }
 
-async function isExists2(file1, file2){
-  fs.stat(file1,(err1) => {
-    if (err1 == null){
-      fs.stat(file2, (err2) => { 
-        return err2 == null ? true : false;            
+async function isExists2(file1, file2) {
+  fs.stat(file1, (err1) => {
+    if (err1 == null) {
+      fs.stat(file2, (err2) => {
+        return err2 == null ? true : false;
       });
     }
     else
-        return false;
+      return false;
   });
 }
